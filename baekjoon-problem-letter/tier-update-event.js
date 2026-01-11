2dc88d8c73283a85f51c4e46a6e8ef1f37ebb156
dohyeon5626 <kodohyeon71@gmail.com>
2026-01-11 22:43:36 +0900

    Update tier update event logic
    #17

--- baekjoon-problem-letter/tier-update-event.js
+++ baekjoon-problem-letter/tier-update-event.js
@@ -1,5 +1,8 @@
 import { findSubscriptionsByNextGeneratedDate, updateSubscriptionProblemInfo } from "./plugin/repository.js";
 import { getNewProblemInfo } from "./service/service.js";
+import { SchedulerClient, CreateScheduleCommand } from "@aws-sdk/client-scheduler";
+
+const client = new SchedulerClient({});
 
 export const run = async (event) => {
     const now = new Date(new Date().getTime() + 9 * 60 * 60 * 1000);
@@ -10,8 +13,9 @@ export const run = async (event) => {
     }).replace(/\./g, '').replace(/\s/g, '-');
 
     const subscriptions = await findSubscriptionsByNextGeneratedDate(dateString);
+    console.log(subscriptions);
     for (const subscription of subscriptions) {
-        if (subscription.sendRound == 0) break;
+        if (subscription.sendRound == 0) return;
         try {
             const problemInfo = await getNewProblemInfo(subscription.userId);
             await updateSubscriptionProblemInfo(subscription.id, problemInfo);
@@ -20,5 +24,33 @@ export const run = async (event) => {
         }
     }
 
-    // TODO 100개 보다 많으면 20분뒤에 더 돌려야함
+    if (subscriptions.length >= 100) {
+        const nextDate = new Date(Date.now() + 20 * 60 * 1000);
+        if(new Date(nextDate + 9 * 60 * 60 * 1000).getHours() >= 6) return;
+
+        const scheduleName = `tier-update-trigger-${nextDate.getTime()}`;
+        const atTime = nextDate.toISOString().slice(0, 19);
+
+        const params = {
+            Name: scheduleName,
+            ScheduleExpression: `at(${atTime})`,
+            ScheduleExpressionTimezone: "UTC",
+            FlexibleTimeWindow: { Mode: "OFF" },
+            
+            Target: {
+                Arn: process.env.TARGET_LAMBDA_ARN,
+                RoleArn: process.env.SCHEDULER_ROLE_ARN
+            },
+            
+            ActionAfterCompletion: "DELETE"
+        }
+
+        try {
+            await client.send(new CreateScheduleCommand(params));
+            console.log(`[Schedule Created] ${scheduleName} at ${atTime}`);
+            return true;
+        } catch (error) {
+            console.error("[Schedule Error]", error);
+        }
+    }
 }
